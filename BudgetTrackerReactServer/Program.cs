968ea6bff41aff03020bfc10c1fede111ab0fdbb
ionutm2004ro <ionut_m2004ro@yahoo.com>
2022-09-16 12:12:19 +0300

    Added Crud API

--- BudgetTrackerReactServer/Program.cs
+++ BudgetTrackerReactServer/Program.cs
@@ -26,4 +26,60 @@ app.UseHttpsRedirection();
 app.MapGet("/get-all-transactions", async () => await TransactionsRepository.GetTransactionsAsync())
     .WithTags("Transactions Endpoints");
 
+app.MapGet("/get-transaction-by-id/{transactionId}", async (int transactionId) =>
+{
+    Transaction transactionToReturn = await TransactionsRepository.GetTransactionByIdAsync(transactionId);
+
+    if (transactionToReturn != null)
+    {
+        return Results.Ok(transactionToReturn);
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Transactions Endpoints");
+
+app.MapPost("/create-transaction", async (Transaction transactionToCreate) =>
+{
+    bool createSuccessful = await TransactionsRepository.CreateTransactionAsync(transactionToCreate);
+
+    if (createSuccessful)
+    {
+        return Results.Ok("Create successful!");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Transactions Endpoints");
+
+app.MapPut("/update-transaction", async (Transaction transactionToUpdate) =>
+{
+    bool updateSuccessful = await TransactionsRepository.UpdateTransactionAsync(transactionToUpdate);
+
+    if (updateSuccessful)
+    {
+        return Results.Ok("Update successful!");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Transactions Endpoints");
+
+app.MapDelete("/delete-transaction-by-id/{transactionId}", async (int postId) =>
+{
+    bool updateSuccessful = await TransactionsRepository.DeleteTransactionAsync(postId);
+
+    if (updateSuccessful)
+    {
+        return Results.Ok("Delete successful!");
+    }
+    else
+    {
+        return Results.BadRequest();
+    }
+}).WithTags("Transactions Endpoints");
+
 app.Run();
